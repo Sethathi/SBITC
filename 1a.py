6eda392e13d3c1bc752658bbfbe2882cdf9133a5
Sethathi Morokole <Sethathi@gmail.com>
2012-02-25 19:40:27 +0200

    Added 1a and 1b

---
+++ 1a.py
@@ -0,0 +1,19 @@
+#!/usr/bin
+
+import math
+
+def is_prime(x):
+	sqrt = int(math.sqrt(x)+1) 
+	for i in range(2,sqrt):
+			if x % i == 0: 
+				return 1
+	
+T = int(input())
+for t in range(1,T+1):
+	count = 0
+	N = int(input())
+	for i in range(1,N+1):
+		if i >1:
+			if not is_prime(i):
+				count = count+1
+	print "Case #%d: %d" % (t, count)
